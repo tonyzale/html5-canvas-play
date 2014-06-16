e716dfa49718ebbb9e555d6b65623ac9fe822631
tonyzale <tonyzale@gmail.com>
2014-06-16 16:37:58 -0500

    Basic Waypoints

--- soccer.js
+++ soccer.js
@@ -1,11 +1,12 @@
-function Soccer(ctx) {
+function Soccer(field) {
   this.players = [];
-  this.ctx = ctx;
+  var soccer = this;
+  field.addEventListener('click', function(){soccer.click()});
+  this.ctx = field.getContext("2d");
   this.players.push(new player(new point(50, 50)));
   this.players.push(new player(new point(100, 50)));
   this.players.push(new player(new point(150, 50)));
   this.timestamp = null;
-  this.soccer = this;
   window.requestAnimationFrame(function(ts) { soccer.timestamp = ts; })
 
   function point(x, y) {
@@ -13,11 +14,44 @@ function Soccer(ctx) {
     this.y = y;
   }
 
+  function DistSquared(p1, p2) {
+    return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+  }
+
+  point.prototype.moveTo = function(p, dist) {
+    if (DistSquared(this, p) <= dist * dist) {
+      this.x = p.x;
+      this.y = p.y;
+      return;
+    }
+    var x_dist = Math.abs(p.x - this.x);
+    var y_dist = Math.abs(p.y - this.y);
+    var x_pct = x_dist / (x_dist + y_dist);
+    if (this.x < p.x) {
+      this.x += x_pct * dist;
+    } else {
+      this.x -= x_pct * dist;
+    }
+    if (this.y < p.y) {
+      this.y += (1 - x_pct) * dist;
+    } else {
+      this.y -= (1 - x_pct) * dist;
+    }
+ }
+
   function player(loc) {
     this.location = loc;
     this.color = "green";
     this.rotation = 0;
+    this.dest = new point(50,50);
   }
+}
+
+Soccer.prototype.click = function() {
+  this.players.forEach(function(p) {
+    p.dest.x = getRandomInt(0,200);
+    p.dest.y = getRandomInt(0,200);
+  });
 }
 
 Soccer.prototype.MainLoop = function(ts) {
@@ -26,8 +60,7 @@ Soccer.prototype.MainLoop = function(ts) {
 
   // game logic
   this.players.forEach(function(p) {
-    p.location.y += step / 160.0;
-    p.rotation += step / 160.0;
+    p.location.moveTo(p.dest, 4);
   });
 
   // render logic
@@ -35,7 +68,7 @@ Soccer.prototype.MainLoop = function(ts) {
   ctx.clearRect(0, 0, 200, 200);
   this.players.forEach(function(p) {
     ctx.fillStyle = p.color;
-    var PLAYER_SIZE = 5;
+    var PLAYER_SIZE = 10;
     ctx.translate(p.location.x, p.location.y);
     ctx.rotate(p.rotation);
     ctx.fillRect(-PLAYER_SIZE*0.5, -PLAYER_SIZE*0.5, PLAYER_SIZE, PLAYER_SIZE);
@@ -49,6 +82,10 @@ Soccer.prototype.MainLoop = function(ts) {
 }
 
 window.onload = function() {
-  var soccer = new Soccer(document.getElementById("field").getContext("2d"));
+  var soccer = new Soccer(document.getElementById("field"));
   window.requestAnimationFrame(function(step){soccer.MainLoop(step)});
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
